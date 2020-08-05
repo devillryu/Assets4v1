efd1abd22e2926b55c7021d6135f11fa807173a6
devillryu <sdryu1@hotmail.com>
2020-08-05 17:46:34 +0700

    Change mainmenu

---
+++ Scirpts/Mainmenu/Swaprole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swaprole : MonoBehaviour
+{
+    public GameObject roledisplayasSurvival;
+    public GameObject roledisplayasHunter;
+    [SerializeField]
+    private string Role = "Survival"; //Dont destroyonload & Instance
+    // Start is called before the first frame update
+    public void Swaproleforplay()
+    {
+        if (Role == "Survival")
+            Role = "Hunter";
+        else
+            Role = "Survival";
+        ChangeDisplay();
+    }
+    public void ChangeDisplay()
+    {
+        if (Role == "Survival")
+        {
+            roledisplayasSurvival.SetActive(true);
+            roledisplayasHunter.SetActive(false);
+        }
+        else
+        {
+            roledisplayasHunter.SetActive(true);
+            roledisplayasSurvival.SetActive(false);
+        }
+    }
+}
