202f5276fbfb1f665a939e9e80156465f17d3d0a
devillryu <sdryu1@hotmail.com>
2020-08-13 10:41:40 +0700

    test

--- Scirpts/Gamemanager.cs
+++ Scirpts/Gamemanager.cs
@@ -4,13 +4,15 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class Gamemanager : MonoBehaviour
+public class Gamemanager : MonoBehaviourPun
 {
     // public Player Playerprefab;
     // [HideInInspector]
     // public Player LocalPlayer;
     // Start is called before the first frame update
+    public Transform[] SpawnPoint;
     void Awake()
     {
         if (!PhotonNetwork.IsConnected)
@@ -26,8 +28,8 @@ public class Gamemanager : MonoBehaviour
     {
         // Player.RefreshInstance(ref LocalPlayer, Playerprefab);
         //Foundbug
+
         CreatePlayer();
-        print("start");
     }
 
     // Update is called once per frame
@@ -45,11 +47,12 @@ public class Gamemanager : MonoBehaviour
     // }
     void CreatePlayer()
     {
+        int spawnPicker = Random.Range(0, SpawnPoint.Length); //Bug
         print(Swaprole.chooserole.Role);
-        if(Swaprole.chooserole.Role == "Survival")
-        PhotonNetwork.Instantiate(Path.Combine("Photonprefabs", "Player"), Vector3.zero, Quaternion.identity);
+        if (Swaprole.chooserole.Role == "Survival")
+            PhotonNetwork.Instantiate(Path.Combine("Photonprefabs", "Player"), SpawnPoint[spawnPicker].position, SpawnPoint[spawnPicker].rotation, 0);
         else
-        PhotonNetwork.Instantiate(Path.Combine("Photonprefabs", "Hunter"), Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate(Path.Combine("Photonprefabs", "Hunter"), SpawnPoint[spawnPicker].position, SpawnPoint[spawnPicker].rotation, 0);
     }
 
 }
